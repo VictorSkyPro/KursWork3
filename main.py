3c2d9582fba5e4705489b52656724e59a4eab09f
Victor.S <vikcnc@mail.ru>
2023-07-13 16:00:19 +0500

    3) sorted() of date

--- main.py
+++ main.py
@@ -3,12 +3,23 @@ from pprint import pprint
 
 
 def main():
-
+    """
+    1) Чтение данных из файла операций.
+    2) Выборка данных со статусом EXECUTED.
+    3) Сортировка по убыванию даты.
+    """
+    # 1) Чтение данных из файла операций.
     with open('operations.json', mode='r', encoding='utf8') as file:
         data = json.load(file)
 
+    # 2) фильтрация данных. Выбираются только со статусом EXECUTED.
+    data = filter(lambda x: x.get('state') == "EXECUTED", data)
 
-    pprint(data)
+    # 3) Сортировка по убыванию даты.
+    data = sorted(data, key=lambda x: x.get('date'), reverse=True)
+
+
+    pprint(list(data))
 
 
 
